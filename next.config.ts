5218b701ff4720061faef4dbd9ad3e61bf613f41
Aureliano2021 <aureliano.oliveira2011@gmail.com>
2025-05-07 16:45:29 -0300

    retirando página estática

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Adiciona suporte para exportação estática
-  trailingSlash: true, // Garante que os caminhos terminem com "/"
-  images: {
-    unoptimized: true, // Desabilita otimização de imagens para exportação estática
-  },
 };
 
 export default nextConfig;
